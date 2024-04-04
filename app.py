6df8d6fb001b16f3f0f583de77c08b7b2076e9ce
unknown <karanbais2701@gmail.com>
2024-04-04 21:36:33 +0530

    train.html added

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.pred_pipeline import CustomData, PredictPipline
+from src.pipeline.training_pipeline import Training_pipeline
 
 application = Flask(__name__)
 
@@ -34,6 +35,33 @@ def predict_datapoint():
         predict_pipeline = PredictPipline()
         results = predict_pipeline.predict(custom_df)
         return render_template ('home.html', result = results[0])
+    
+@app.route('/upload', methods=['GET','POST'])
+def upload_file():
+    # Check if the POST request has the file part
+    if request.method == 'GET':
+        return render_template('train.html')
+    else:
+        if 'file' not in request.files:
+            return jsonify({'error': 'No file part'})
+
+        file = request.files['file']
+
+        if file.filename == '':
+            return jsonify({'error': 'No selected file'})
+
+        if file and file.filename.endswith('.csv'):
+       
+            df = pd.read_csv(file)
+
+            train = Training_pipeline()
+            r2score = train.Training(df)
+
+            return render_template('train.html', result= r2score)
+        else:
+            return jsonify({'error': 'Invalid file format'})
+        
+
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
